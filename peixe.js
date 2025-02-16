d5340a6ffc8c51c42951ccdbb81c8b89ef40826f
Stefanne Victória Soares <stefanne.soares@sou.inteli.edu.br>
2025-02-16 19:58:14 -0300

    comentando o código

--- peixe.js
+++ peixe.js
@@ -1,36 +1,39 @@
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 800, // Largura da tela do jogo
+    height: 600, // Altura da tela do jogo
 
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
+    scene: {  // Define as funções principais do jogo
+        preload: preload, // Carrega os arquivos antes do jogo iniciar 
+        create: create, // Cria os elemntos na tela
+        update: update // Atualiza o jogo
     }
 };
 
 var game = new Phaser.Game(config)
 
-var peixinho;
+// Variáveis para armazenar elementos do jogo
+var peixinho; // Variável para o peixe
+var musica; // Variável para a música
 
-var musica;
-
+// Carrega os arquivos
 function preload() {
-    this.load.image('mar', 'assets/bg_azul-claro.png');
-    this.load.image('logo', 'assets/logo-inteli_azul.png');
-    this.load.image('peixe', 'assets/peixes/carpa.png');
-    this.load.audio('musica', 'assets/musica.mp3');
+    this.load.image('mar', 'assets/bg_azul-claro.png'); // Carrega a imagem de fundo
+    this.load.image('logo', 'assets/logo-inteli_azul.png'); // Carrega a logo do Inteli
+    this.load.image('peixe', 'assets/peixes/carpa.png'); // Carrega a imagem do peixe
+    this.load.audio('musica', 'assets/musica.mp3'); // Carrega a música
 } 
 
+// Adiciona os elementos na tela
 function create() {
-    this.add.image(400, 300, 'mar');
-    this.add.image(400, 525, 'logo').setScale(0.4);
-    peixinho = this.add.image(400,300, 'peixe');
+    this.add.image(400, 300, 'mar'); // Adiciona o fundo
+    this.add.image(400, 525, 'logo').setScale(0.4); // Adiciona a logo
+    peixinho = this.add.image(400,300, 'peixe'); // Adiciona o peixe
         peixinho.setOrigin(0.5,0.5);
         peixinho.setFlip(true,false);
-        peixinho.setScale(0.7); 
+        peixinho.setScale(0.7); // Reduz o tamanho do peixe
 
+    // Adiciona e toca a música em loop 
     musica = this.sound.add('musica');
         musica.play();
 
@@ -44,6 +47,7 @@ function create() {
     });
 }
 
+// Atualiza a posição do peixe de acordo com o mouse
 function update() {
     peixinho.x = this.input.x;
     peixinho.y = this.input.y;
